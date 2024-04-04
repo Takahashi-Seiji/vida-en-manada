1f183b83c7ac355045e28d6f15183ffe9ab76bf6
seijitakahashi <takahashiseiji18@gmail.com>
2024-04-04 14:01:14 -0600

    Add validations to the user registration config

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, :phone_number, :location, presence: true
+  validates :phone_number, uniqueness: true
+  validates :email, uniqueness: true
+  validates :birth_date, presence: true
 end
