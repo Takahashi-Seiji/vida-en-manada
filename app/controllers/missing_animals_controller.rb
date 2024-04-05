8ce46831d8b174c229d7a02522ac3c30187187ac
seijitakahashi <takahashiseiji18@gmail.com>
2024-04-04 18:00:18 -0600

    Finish with CRUD actions for reporting a missing animal

--- app/controllers/missing_animals_controller.rb
+++ app/controllers/missing_animals_controller.rb
@@ -17,6 +17,26 @@ class MissingAnimalsController < ApplicationController
     @user = @missing_animal.user
   end
 
+  def edit
+    @missing_animal = MissingAnimal.find(params[:id])
+  end
+
+  def update
+    @missing_animal = MissingAnimal.find(params[:id])
+    if @missing_animal.update(missing_animal_params)
+      redirect_to missing_animal_path(@missing_animal)
+    end
+  end
+
+  def destroy
+    @missing_animal = MissingAnimal.find(params[:id])
+    if @missing_animal.destroy
+      redirect_to root_path
+    else
+      raise
+    end
+  end
+
   private
 
   def missing_animal_params
