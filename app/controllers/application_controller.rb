619fc3ebc1fe7247b9854045ff32b022c6db6aa2
seijitakahashi <takahashiseiji18@gmail.com>
2024-04-04 21:22:29 -0600

    hotfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :location, :birth_date])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :location, :birth_date])
   end
+
+  def authenticate_user!
+    redirect_to new_user_registration_path unless user_signed_in?
+  end
 end
