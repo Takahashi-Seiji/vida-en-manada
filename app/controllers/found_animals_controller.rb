9baae33d3b1cd0a3900dd292ada09d30339b4863
Seiji Takahashi Sánchez <140453075+Takahashi-Seiji@users.noreply.github.com>
2024-04-08 12:49:34 -0600

    Add CRUD actions for FoundAnimal model (#13)

--- app/controllers/found_animals_controller.rb
+++ app/controllers/found_animals_controller.rb
@@ -1,2 +1,43 @@
 class FoundAnimalsController < ApplicationController
+  def new
+    @found_animal = FoundAnimal.new
+  end
+
+  def create
+    @found_animal = FoundAnimal.new(found_animal_params)
+    @found_animal.user = current_user
+
+    if @found_animal.save
+      redirect_to found_animal_path(@found_animal)
+    end
+  end
+
+  def show
+    @found_animal = FoundAnimal.find(params[:id])
+    @user = @found_animal.user
+  end
+
+  def edit
+    @found_animal = FoundAnimal.find(params[:id])
+  end
+
+  def update
+    @found_animal = FoundAnimal.find(params[:id])
+    if @found_animal.update(found_animal_params)
+      redirect_to found_animal_path(@found_animal)
+    end
+  end
+
+  def destroy
+    @found_animal = FoundAnimal.find(params[:id])
+    if @found_animal.destroy
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def found_animal_params
+    params.require(:found_animal).permit(:species, :age, :name, :breed, :found_location, :found_date, :sterilized, :size, :color, :description)
+  end
 end
