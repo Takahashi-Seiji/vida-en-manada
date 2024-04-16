5768f2de806beeafb1d6cd5f2cd2eaca704560a8
seijitakahashi <takahashiseiji18@gmail.com>
2024-04-15 18:59:50 -0600

    hotfix

--- db/migrate/20240415191355_add_users_to_chatrooms.rb
+++ db/migrate/20240415191355_add_users_to_chatrooms.rb
@@ -1,14 +1,7 @@
 class AddUsersToChatrooms < ActiveRecord::Migration[6.0]
   def up
-    add_reference :chatrooms, :user1, null: true, foreign_key: { to_table: :users }
-    add_reference :chatrooms, :user2, null: true, foreign_key: { to_table: :users }
-
-    # Assuming User.first can be used as a default user
-    default_user = User.first
-    Chatroom.update_all(user1_id: default_user.id, user2_id: default_user.id)
-
-    change_column_null :chatrooms, :user1_id, false
-    change_column_null :chatrooms, :user2_id, false
+    add_reference :chatrooms, :user1, foreign_key: { to_table: :users }
+    add_reference :chatrooms, :user2, foreign_key: { to_table: :users }
   end
 
   def down
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_15_191355) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_15_235419) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,8 +46,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_15_191355) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user1_id", null: false
-    t.bigint "user2_id", null: false
+    t.bigint "user1_id"
+    t.bigint "user2_id"
     t.index ["user1_id"], name: "index_chatrooms_on_user1_id"
     t.index ["user2_id"], name: "index_chatrooms_on_user2_id"
   end
