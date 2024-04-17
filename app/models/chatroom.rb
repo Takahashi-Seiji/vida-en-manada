ee721163f8687fe0634cce33a29e8f4c2b7b9c1d
Seiji Takahashi Sánchez <140453075+Takahashi-Seiji@users.noreply.github.com>
2024-04-17 15:21:48 -0600

    active chats feat (#34)

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,4 +2,5 @@ class Chatroom < ApplicationRecord
   has_many :messages, dependent: :destroy
   belongs_to :user1, class_name: "User"
   belongs_to :user2, class_name: "User"
+  belongs_to :missing_animal
 end
