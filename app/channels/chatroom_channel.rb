de47628f6b4e446125768c3e28fd4d1a850c0eb8
Seiji Takahashi Sánchez <140453075+Takahashi-Seiji@users.noreply.github.com>
2024-04-18 21:28:30 -0600

    Some front for fun (#38)
    * added front end to chatrooms
    
    * added that the chat starts with the scrollbar to the bottom messages

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -7,4 +7,10 @@ class ChatroomChannel < ApplicationCable::Channel
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
+
+  def speak(data)
+    message = Message.create!(content: data['message'], user_id: current_user.id, chatroom_id: data['chatroom_id'])
+    template = ApplicationController.renderer.render(message)
+    ActionCable.server.broadcast("chatroom_#{data['chatroom_id']}", { user_id: current_user.id, message: template })
+  end
 end
--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -3,26 +3,45 @@ import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
-  static values = { chatroomId: Number }
+  static values = { chatroomId: Number, currentUserId: Number}
   static targets = ["messages", "form"]
 
   connect() {
     console.log(`Connected to chatroom subscription controller" ${this.chatroomIdValue}`);
+    console.log(`Current user id: ${this.currentUserIdValue}`);
     this.subscription = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
       {
         received: data => {
           console.log("Received data from chatroom channel", data);
-          this.#insertMessageAndScrollDown(data);
+          const parser = new DOMParser();
+          const htmlDocument = parser.parseFromString(data, "text/html");
+          const messageElement = htmlDocument.querySelector(".message");
+          const userId = messageElement ? messageElement.dataset.userId : undefined;
+          this.#insertMessageAndScrollDown(userId, data);
           this.resetForm();
+          this.scrollToBottom();
         }
       }
     )
     console.log(`Subscribed to chatroom channel ${this.chatroomIdValue}`);
+    this.scrollToBottom();
   }
 
-  #insertMessageAndScrollDown(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+  scrollToBottom() {
+    this.messagesTarget.scrollTop = this.messagesTarget.scrollHeight;
+  }
+
+  #insertMessageAndScrollDown(user_id, message) {
+    if (this.currentUserIdValue === undefined) {
+      console.error('currentUserIdValue is undefined');
+      return;
+    }
+
+    const currentUser = this.currentUserIdValue;
+    const messageClass = user_id == currentUser ? 'message-right' : 'message-left';
+    const messageHTML = `<div class="${messageClass}">${message}</div>`;
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageHTML)
     console.log("Scrolled to the bottom of the chatroom", this.messagesTarget.scrollHeight);
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
