8a53708107fceff341dc0fb9514860b2597815fc
seijitakahashi <takahashiseiji18@gmail.com>
2024-04-22 12:42:11 -0600

    hotfix for heroku

--- db/migrate/20240415235419_add_latitude_and_longitude_to_missing_animals.rb
+++ db/migrate/20240415235419_add_latitude_and_longitude_to_missing_animals.rb
@@ -1,6 +1,6 @@
 class AddLatitudeAndLongitudeToMissingAnimals < ActiveRecord::Migration[7.1]
   def change
-    add_column :missing_animals, :latitude, :float
-    add_column :missing_animals, :longitude, :float
+    # add_column :missing_animals, :latitude, :float
+    # add_column :missing_animals, :longitude, :float
   end
 end
